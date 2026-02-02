4136dd81686ba5afb39e148248adb5c425f1956d
phuong.doan <phuong.doan@s3corp.com.vn>
2026-02-02 23:30:22 +0700

    initial commit

---
+++ todo-app/todo_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  todo_appApp.swift
+//  todo-app
+//
+//  Created by Doan Phuong on 2/2/26.
+//
+
+import SwiftUI
+
+@main
+struct todo_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
